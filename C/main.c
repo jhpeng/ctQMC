7fa9919b41f810512e08a43262d05ee57356e872
jhpeng <alanpeng1995@gmail.com>
2021-01-27 19:09:46 +0800

    test jq3_ladder_square model

---
+++ C/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <gsl/gsl_rng.h>
+
+#include "dtype.h"
+#include "models.h"
+#include "update.h"
+#include "stats.h"
+
+int main() {
+    int x = 8;
+    int y = 8;
+    double beta = 10;
+    double q3 = 1.5;
+    unsigned long int seed = 33474234;
+    int thermal = 1000;
+
+    gsl_rng* rng = gsl_rng_alloc(gsl_rng_mt19937);
+    gsl_rng_set(rng,seed);
+
+    model* m = jq3_ladder_square(x,y,q3);
+    world_line* w = malloc_world_line(1024,2*(m->mhnspin),m->nsite);
+
+    estimator* samples[6];
+    samples[0] = malloc_estimator(100000,"ms1");
+    samples[1] = malloc_estimator(100000,"ms2");
+    samples[2] = malloc_estimator(100000,"ms4");
+    samples[3] = malloc_estimator(100000,"Xs");
+    samples[4] = malloc_estimator(100000,"Xu");
+    samples[5] = malloc_estimator(100000,"Noo");
+
+    w->beta = beta;
+    for(int i=0;i<(w->nsite);i++) {
+        w->istate[i] = 1;
+        if(gsl_rng_uniform_pos(rng)<0.5) {
+            w->istate[i] = -1;
+        }
+    }
+
+    for(int i=0;i<thermal;i++) {
+        remove_vertices(w);
+        insert_vertices(w,m,rng);
+        clustering(w,m);
+        flip_cluster(w,rng);
+    }
+}
