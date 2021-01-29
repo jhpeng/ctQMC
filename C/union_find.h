671bb2e72a4aa672047a5ab9c62321cb05502d2f
jhpeng <alanpeng1995@gmail.com>
2021-01-30 02:50:22 +0800

    parallelize union-find algorithm

--- C/union_find.h
+++ C/union_find.h
@@ -3,8 +3,10 @@
 
 int root(int* p, int v);
 
-void compress(int* p, int v, int r);
-
 void merge(int* p, int* w, int va, int vb);
+
+int root_parallel(int* p, int v);
+
+void merge_parallel(int* p, int* w, int va, int vb);
 
 #endif
