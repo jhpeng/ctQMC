9b1349acf6aac72be685a74458479ffb0631c9ff
jhpeng <alanpeng1995@gmail.com>
2021-02-04 17:54:00 +0800

    parallelize jq3_ladder_square model

---
+++ C/projects/parallel/jq3_ladder_square/main.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <gsl/gsl_rng.h>
+
+#include "dtype.h"
+#include "models.h"
+#include "update_omp.h"
+#include "stats.h"
+
+int Lx;
+int Ly;
+double Q3;
+double Beta;
+unsigned long int Seed;
+
+double* staggered_structure=NULL;
+void measurement(world_line_omp* w, model* m, estimator** samples) {
+    if(staggered_structure==NULL) {
+        staggered_structure = (double*)malloc(sizeof(double)*w->nsite);
+        for(int j=0;j<Ly;j++) {
+            for(int i=0;i<Lx;i++) {
+                int i_site = i+j*Lx;
+                staggered_structure[i_site] = ((i+j)%2)*2-1;
+            }
+        }
+    }
+
+    int mhnspin = m->mhnspin;
+
+    double mz = 0;
+    double ms = 0;
+    double msx = 0;
+    double ms1 = 0;
+    double ms2 = 0;
+    double ms4 = 0;
+    double chiu = 0;
+
+    int nsite = w->nsite;
+    for(int i=0;i<nsite;i++) {
+        mz += w->istate[i];
+        ms += (w->istate[i])*staggered_structure[i];
+        w->pstate[i] = w->istate[i];
+    }
+
+    int l=0;
+    for(int i_thread=0;i_thread<(w->nthread);i_thread++) {
+
+        vertex* sequence = w->sequenceB[i_thread];
+        if(w->flag[i_thread])
+            sequence = w->sequenceA[i_thread];
+
+        for(int i=0;i<(w->len[i_thread]);i++) {
+            int bond   = (sequence[i]).bond;
+            int hNspin = (sequence[i]).hNspin;
+        
+            for(int j=0;j<hNspin;j++) {
+                int i_site = m->bond2index[bond*mhnspin+j];
+                int statep = (sequence[i]).state[j];
+                int staten = (sequence[i]).state[j+hNspin];
+                int dif    = (statep*staten-1)*statep;
+                ms += staggered_structure[i_site]*dif;
+                w->pstate[i_site] = staten;
+            }
+
+            msx += ms;
+            ms1 += abs(ms);
+            ms2 += ms*ms;
+            ms4 += ms*ms*ms*ms;
+        }
+
+        l += w->len[i_thread];
+    }
+
+
+    chiu = mz*mz*Beta*0.25/nsite;
+    if(l!=0) {
+        msx = Beta*(msx*msx+ms2)*0.25/l/(l+1)/nsite;
+        ms1 = ms1*0.5/nsite/l;
+        ms2 = ms2*0.25/nsite/nsite/l;
+        ms4 = ms4*0.0625/nsite/nsite/nsite/nsite/l;
+    } else {
+        msx = Beta*ms*ms/2.0/nsite*0.25;
+        ms1 = abs(ms)*0.5/nsite;
+        ms2 = ms*ms*0.25/nsite/nsite;
+        ms4 = ms*ms*ms*ms/0.0625/nsite/nsite/nsite/nsite;
+    }
+
+    append_estimator(samples[0],ms1);
+    append_estimator(samples[1],ms2);
+    append_estimator(samples[2],ms4);
+    append_estimator(samples[3],msx);
+    append_estimator(samples[4],chiu);
+    append_estimator(samples[5],l);
+}
+
+int main() {
+    Lx = 32;
+    Ly = 32;
+    Beta = 10;
+    Q3 = 1.5;
+    Seed = 84273003;
+
+    int thermal = 100000;
+    int nsweep  = 1000000;
+    int nthread = 4;
+
+    gsl_rng* rng[nthread];
+    for(int i=0;i<nthread;i++) {
+        rng[i] = gsl_rng_alloc(gsl_rng_mt19937);
+        gsl_rng_set(rng[i],(unsigned long int)(Seed*(i+1)*3.14159265359));
+    }
+
+    model* m = jq3_ladder_square(Lx,Ly,Q3);
+    world_line_omp* w = malloc_world_line_omp(1024,2*(m->mhnspin),(m->nsite),nthread);
+
+    estimator* samples[6];
+    samples[0] = malloc_estimator(nsweep,"ms1");
+    samples[1] = malloc_estimator(nsweep,"ms2");
+    samples[2] = malloc_estimator(nsweep,"ms4");
+    samples[3] = malloc_estimator(nsweep,"Xs");
+    samples[4] = malloc_estimator(nsweep,"Xu");
+    samples[5] = malloc_estimator(nsweep,"Noo");
+
+    w->beta = Beta;
+    for(int i=0;i<(w->nsite);i++) {
+        w->istate[i] = 1;
+        if(gsl_rng_uniform_pos(rng[0])<0.5) {
+            w->istate[i] = -1;
+        }
+
+        for(int j=1;j<nthread;j++) 
+            w->istate[i+(w->nsite)*j] = w->istate[i];
+    }
+
+    for(int i=0;i<thermal;i++) {
+        remove_vertices_omp(w);
+        insert_vertices_omp(w,m,rng);
+        clustering_inner_omp(w,m);
+        clustering_crossing(w);
+        flip_cluster_omp(w,rng);
+    }
+
+    int i=0;
+    int nblock=0;
+    while(1) {
+        remove_vertices_omp(w);
+        insert_vertices_omp(w,m,rng);
+        clustering_inner_omp(w,m);
+        clustering_crossing(w);
+        flip_cluster_omp(w,rng);
+        measurement(w,m,samples);
+        i++;
+        
+        if(i>1024 && nblock!=(samples[0])->nblock){
+            nblock = (samples[0])->nblock;
+            printf("=========================================\n");
+            printf("L=%d q=%.4f beta=%.1f\n",Lx,Q3,Beta);
+            print_detail(samples[1]);
+            print_detail(samples[3]);
+            print_detail(samples[4]);
+            print_detail(samples[5]);
+        }
+    }
+
+    free_model(m);
+    free_world_line_omp(w);
+    free_estimator(samples[0]);
+    free_estimator(samples[1]);
+    free_estimator(samples[2]);
+    free_estimator(samples[3]);
+    free_estimator(samples[4]);
+    free_estimator(samples[5]);
+
+    for(int i=0;i<nthread;i++)
+        gsl_rng_free(rng[i]);
+}
