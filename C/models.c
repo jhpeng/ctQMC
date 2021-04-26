3b12df8c27805012a60f8787faada38d3e1d4079
jhpeng <alanpeng1995@gmail.com>
2021-04-26 14:48:13 +0800

    implement the qlmodel on 2d square lattice

--- C/models.c
+++ C/models.c
@@ -90,6 +90,93 @@ int insert_rule_singlet_triplet_proj_2_1(int* state) {
     return 0;
 }
 
+/* graph name : spatial_2_2
+//     4    5  6    7
+//     |    |  |    |
+//     |----|  |----|
+//     |    |  |    |
+//     0    1  2    3
+*/
+
+int link_rule_spatial_2_2[]
+        = {0,0,2,2,0,0,2,2,4,1,4,1,1,1,1,1};
+
+/* graph name : spatial_4
+//     4    5    6    7
+//     |    |    |    |
+//     |----|----|----|
+//     |    |    |    |
+//     0    1    2    3
+*/
+
+int link_rule_spatial_4[]
+        = {0,0,0,0,0,0,0,0,8,1,1,1,1,1,1,1};
+
+/* graph name : single_box_vertex_1
+//     4    5    6    7
+//     |____|....|____|
+//      ____ .... ____
+//     |    |    |    |
+//     0    1    2    3 
+*/
+
+int link_rule_single_box_vertex_1[]
+        = {0,0,0,0,4,4,4,4,4,1,1,1,4,1,1,1};
+
+int insert_rule_single_box_vertex_1(int* state){
+    if(state[0]*state[1]==1){
+        if(state[2]*state[3]==1){
+            if(state[1]*state[2]==-1){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+/* graph name : single_box_vertex_2
+//     4    5    6    7
+//     |____|....|____|
+//     |    |    |    |
+//     0    1    2    3 
+*/
+
+int link_rule_single_box_vertex_2[]
+        = {0,0,0,0,0,0,0,0,8,1,1,1,1,1,1,1};
+
+int insert_rule_single_box_vertex_2(int* state){
+    if(state[0]*state[1]==1){
+        if(state[2]*state[3]==1){
+            if(state[1]*state[2]==-1){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+/* graph name : single_box_non_vertex
+//     4    5    6    7
+//     |    |    |    |
+//     |----|----|----|
+//     |    |    |    |
+//     0    1    2    3 
+*/
+
+int link_rule_single_box_non_vertex[]
+        = {0,0,0,0,0,0,0,0,8,1,1,1,1,1,1,1};
+
+int insert_rule_single_box_non_vertex(int* state){
+    if(state[0]*state[1]==1){
+        if(state[2]*state[3]==1){
+            if(state[1]*state[2]==-1){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
 model* jq3_ladder_square(int lx, int ly, double q) {
     int nsite = lx*ly;
     int nbond = 4*nsite;
@@ -476,3 +563,142 @@ model* jq3_ladder_square_impurity_spin_one(int lx, int ly, double q) {
 
     return m;
 }
+
+model* quantum_link_model_2d_square(int lx, int ly, double lambda) {
+    int nsite = 2*lx*ly;
+    int nbond = 3*lx*ly;
+    int mhnspin = 4;
+    model* m = malloc_model(nsite,nbond+4*lx*ly,mhnspin);
+
+    int n=0;
+    for(int y=0;y<ly;y++) {
+        for(int x=0;x<lx;x++) {
+            int u1 = x+y*lx;
+            int u2 = (x+1)%lx+y*lx+lx*ly;
+            int u3 = x+((y+1)%ly)*lx;
+            int u4 = x+y*lx+lx*ly;
+
+            m->bond2type[n]   = 0;
+            m->bond2hNspin[n] = 4;
+            m->bond2weight[n] = 1.0;
+            m->sweight += 1.0;
+            m->bond2index[n*mhnspin+0] = u1;
+            m->bond2index[n*mhnspin+1] = u2;
+            m->bond2index[n*mhnspin+2] = u3;
+            m->bond2index[n*mhnspin+3] = u4;
+            n++;
+        }
+    }
+    for(int y=0;y<ly;y++) {
+        for(int x=0;x<lx;x++) {
+            int u1 = x+y*lx;
+            int u2 = (x+1)%lx+y*lx+lx*ly;
+            int u3 = x+((y+1)%ly)*lx;
+            int u4 = x+y*lx+lx*ly;
+
+            m->bond2type[n]   = 1;
+            m->bond2hNspin[n] = 4;
+            m->bond2weight[n] = lambda;
+            m->sweight += lambda;
+            m->bond2index[n*mhnspin+0] = u1;
+            m->bond2index[n*mhnspin+1] = u2;
+            m->bond2index[n*mhnspin+2] = u3;
+            m->bond2index[n*mhnspin+3] = u4;
+            n++;
+        }
+    }
+    for(int y=0;y<ly;y++) {
+        for(int x=0;x<lx;x++) {
+            int u1 = x+y*lx;
+            int u2 = (x+1)%lx+y*lx+lx*ly;
+            int u3 = x+((y+1)%ly)*lx;
+            int u4 = x+y*lx+lx*ly;
+
+            m->bond2type[n]   = 2;
+            m->bond2hNspin[n] = 4;
+            m->bond2weight[n] = 1.0;
+            m->sweight += 1.0;
+            m->bond2index[n*mhnspin+0] = u1;
+            m->bond2index[n*mhnspin+1] = u2;
+            m->bond2index[n*mhnspin+2] = u3;
+            m->bond2index[n*mhnspin+3] = u4;
+            n++;
+        }
+    }
+    for(int y=0;y<ly;y++) {
+        for(int x=0;x<lx;x++) {
+            int u1 = x+y*lx;
+            int u2 = x+y*lx+lx*ly;
+            int u3 = (x+lx-1)%lx+y*lx;
+            int u4 = x+((y+ly-1)%ly)*lx+lx+ly;
+
+            m->bond2type[n]   = 3;
+            m->bond2hNspin[n] = 4;
+            m->bond2weight[n] = 0.0;
+            m->sweight += 0.0;
+            m->bond2index[n*mhnspin+0] = u1;
+            m->bond2index[n*mhnspin+1] = u4;
+            m->bond2index[n*mhnspin+2] = u2;
+            m->bond2index[n*mhnspin+3] = u3;
+            n++;
+
+            m->bond2type[n]   = 3;
+            m->bond2hNspin[n] = 4;
+            m->bond2weight[n] = 0.0;
+            m->sweight += 0.0;
+            m->bond2index[n*mhnspin+0] = u1;
+            m->bond2index[n*mhnspin+1] = u2;
+            m->bond2index[n*mhnspin+2] = u3;
+            m->bond2index[n*mhnspin+3] = u4;
+            n++;
+
+            m->bond2type[n]   = 3;
+            m->bond2hNspin[n] = 4;
+            m->bond2weight[n] = 0.0;
+            m->sweight += 0.0;
+            m->bond2index[n*mhnspin+0] = u1;
+            m->bond2index[n*mhnspin+1] = u3;
+            m->bond2index[n*mhnspin+2] = u2;
+            m->bond2index[n*mhnspin+3] = u4;
+            n++;
+
+            m->bond2type[n]   = 4;
+            m->bond2hNspin[n] = 4;
+            m->bond2weight[n] = 0.0;
+            m->sweight += 0.0;
+            m->bond2index[n*mhnspin+0] = u1;
+            m->bond2index[n*mhnspin+1] = u2;
+            m->bond2index[n*mhnspin+2] = u3;
+            m->bond2index[n*mhnspin+3] = u4;
+            n++;
+        }
+    }
+
+    for(int i=0;i<4*mhnspin;i++) {
+        m->link[0*4*mhnspin+i] = link_rule_single_box_vertex_1[i];
+    }
+    for(int i=0;i<4*mhnspin;i++) {
+        m->link[1*4*mhnspin+i] = link_rule_single_box_vertex_2[i];
+    }
+    for(int i=0;i<4*mhnspin;i++) {
+        m->link[2*4*mhnspin+i] = link_rule_single_box_non_vertex[i];
+    }
+    for(int i=0;i<4*mhnspin;i++) {
+        m->link[3*4*mhnspin+i] = link_rule_spatial_2_2[i];
+    }
+    for(int i=0;i<4*mhnspin;i++) {
+        m->link[4*4*mhnspin+i] = link_rule_spatial_4[i];
+    }
+
+    m->insert[0] = insert_rule_single_box_vertex_1;
+    m->insert[1] = insert_rule_single_box_vertex_2;
+    m->insert[2] = insert_rule_single_box_non_vertex;
+
+    m->nsite = nsite;
+    m->nbond = nbond;
+    m->mhnspin = mhnspin;
+    create_cmf(m->cmf,m->bond2weight,nbond);
+
+    return m;
+}
+
--- C/models.h
+++ C/models.h
@@ -9,4 +9,6 @@ model* jq3_ladder_square_impurity_spin_half(int lx, int ly, double q);
 
 model* jq3_ladder_square_impurity_spin_one(int lx, int ly, double q);
 
+model* quantum_link_model_2d_square(int lx, int ly, double lambda);
+
 #endif
