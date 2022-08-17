dd3fbd5d299ac08e757bf14e9503d1b94a71340b
jhpeng <alanpeng1995@gmail.com>
2022-08-18 01:05:53 +0800

    qlm cubic

--- C/models.h
+++ C/models.h
@@ -11,4 +11,6 @@ model* jq3_ladder_square_impurity_spin_one(int lx, int ly, double q);
 
 model* quantum_link_model_2d_square(int lx, int ly, double lambda);
 
+model* quantum_link_model_3d_cubic(int lx, int ly, int lz, double lambda);
+
 #endif
